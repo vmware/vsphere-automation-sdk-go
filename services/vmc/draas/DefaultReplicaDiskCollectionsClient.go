c500a871becc40fe6cb3ba209ecd59c483d58dd1
Vandit Mehta <vmehta@vmware.com>
2020-02-21 11:39:52 +0530

    Adding draas services to Go SDK.

---
+++ services/vmc/draas/DefaultReplicaDiskCollectionsClient.go
@@ -0,0 +1,140 @@
+
+/* Copyright © 2019 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: BSD-2-Clause */
+
+// Code generated. DO NOT EDIT.
+
+/*
+ * Client stubs for service: ReplicaDiskCollections
+ * Functions that implement the generated ReplicaDiskCollectionsClient interface
+ */
+
+
+package draas
+
+import (
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/lib/vapi/std/errors"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/services/vmc/draas/model"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/bindings"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/core"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/data"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/lib"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/log"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/protocol/client"
+)
+
+type DefaultReplicaDiskCollectionsClient struct {
+	interfaceName       string
+	interfaceDefinition core.InterfaceDefinition
+	methodIdentifiers   []core.MethodIdentifier
+	methodNameToDefMap  map[string]*core.MethodDefinition
+	errorBindingMap     map[string]bindings.BindingType
+	interfaceIdentifier core.InterfaceIdentifier
+	connector           client.Connector
+}
+
+func NewDefaultReplicaDiskCollectionsClient(connector client.Connector) *DefaultReplicaDiskCollectionsClient {
+	interfaceName := "com.vmware.vmc.draas.replica_disk_collections"
+	interfaceIdentifier := core.NewInterfaceIdentifier(interfaceName)
+	methodIdentifiers := []core.MethodIdentifier{
+		core.NewMethodIdentifier(interfaceIdentifier, "get"),
+	}
+	interfaceDefinition := core.NewInterfaceDefinition(interfaceIdentifier, methodIdentifiers)
+	errorBindingMap := make(map[string]bindings.BindingType)
+	errorBindingMap[errors.InternalServerError{}.Error()] = errors.InternalServerErrorBindingType()
+	errorBindingMap[errors.InvalidArgument{}.Error()] = errors.InvalidArgumentBindingType()
+	errorBindingMap[errors.OperationNotFound{}.Error()] = errors.OperationNotFoundBindingType()
+	errorBindingMap[errors.UnexpectedInput{}.Error()] = errors.UnexpectedInputBindingType()
+	errorBindingMap[errors.ServiceUnavailable{}.Error()] = errors.ServiceUnavailableBindingType()
+	errorBindingMap[errors.TimedOut{}.Error()] = errors.TimedOutBindingType()
+	rIface := DefaultReplicaDiskCollectionsClient{interfaceName: interfaceName, methodIdentifiers: methodIdentifiers, interfaceDefinition: interfaceDefinition, errorBindingMap: errorBindingMap, interfaceIdentifier: interfaceIdentifier, connector: connector}
+	rIface.methodNameToDefMap = make(map[string]*core.MethodDefinition)
+	rIface.methodNameToDefMap["get"] = rIface.getMethodDefinition()
+	return &rIface
+}
+
+func (rIface *DefaultReplicaDiskCollectionsClient) Get(orgParam string, sddcParam string, datastoreMoIdParam *string) ([]model.ReplicaDiskCollection, error) {
+	typeConverter := rIface.connector.TypeConverter()
+	methodIdentifier := core.NewMethodIdentifier(rIface.interfaceIdentifier, "get")
+	sv := bindings.NewStructValueBuilder(replicaDiskCollectionsGetInputType(), typeConverter)
+	sv.AddStructField("Org", orgParam)
+	sv.AddStructField("Sddc", sddcParam)
+	sv.AddStructField("DatastoreMoId", datastoreMoIdParam)
+	inputDataValue, inputError := sv.GetStructValue()
+	if inputError != nil {
+		var emptyOutput []model.ReplicaDiskCollection
+		return emptyOutput, bindings.VAPIerrorsToError(inputError)
+	}
+	operationRestMetaData := replicaDiskCollectionsGetRestMetadata()
+	connectionMetadata := map[string]interface{}{lib.REST_METADATA: operationRestMetaData}
+	rIface.connector.SetConnectionMetadata(connectionMetadata)
+	methodResult := rIface.Invoke(rIface.connector.NewExecutionContext(), methodIdentifier, inputDataValue)
+	var emptyOutput []model.ReplicaDiskCollection
+	if methodResult.IsSuccess() {
+		output, errorInOutput := typeConverter.ConvertToGolang(methodResult.Output(), replicaDiskCollectionsGetOutputType())
+		if errorInOutput != nil {
+			return emptyOutput, bindings.VAPIerrorsToError(errorInOutput)
+		}
+		return output.([]model.ReplicaDiskCollection), nil
+	} else {
+		methodError, errorInError := typeConverter.ConvertToGolang(methodResult.Error(), rIface.errorBindingMap[methodResult.Error().Name()])
+		if errorInError != nil {
+			return emptyOutput, bindings.VAPIerrorsToError(errorInError)
+		}
+		return emptyOutput, methodError.(error)
+	}
+}
+
+
+func (rIface *DefaultReplicaDiskCollectionsClient) Invoke(ctx *core.ExecutionContext, methodId core.MethodIdentifier, inputDataValue data.DataValue) core.MethodResult {
+	methodResult := rIface.connector.GetApiProvider().Invoke(rIface.interfaceName, methodId.Name(), inputDataValue, ctx)
+	return methodResult
+}
+
+
+func (rIface *DefaultReplicaDiskCollectionsClient) getMethodDefinition() *core.MethodDefinition {
+	interfaceIdentifier := core.NewInterfaceIdentifier(rIface.interfaceName)
+	typeConverter := rIface.connector.TypeConverter()
+
+	input, inputError := typeConverter.ConvertToDataDefinition(replicaDiskCollectionsGetInputType())
+	output, outputError := typeConverter.ConvertToDataDefinition(replicaDiskCollectionsGetOutputType())
+	if inputError != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultReplicaDiskCollectionsClient.get method's input - %s",
+			bindings.VAPIerrorsToError(inputError).Error())
+		return nil
+	}
+	if outputError != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultReplicaDiskCollectionsClient.get method's output - %s",
+			bindings.VAPIerrorsToError(outputError).Error())
+		return nil
+	}
+	methodIdentifier := core.NewMethodIdentifier(interfaceIdentifier, "get")
+	errorDefinitions := make([]data.ErrorDefinition, 0)
+	rIface.errorBindingMap[errors.Unauthenticated{}.Error()] = errors.UnauthenticatedBindingType()
+	errDef1, errError1 := typeConverter.ConvertToDataDefinition(errors.UnauthenticatedBindingType())
+	if errError1 != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultReplicaDiskCollectionsClient.get method's errors.Unauthenticated error - %s",
+			bindings.VAPIerrorsToError(errError1).Error())
+		return nil
+	}
+	errorDefinitions = append(errorDefinitions, errDef1.(data.ErrorDefinition))
+	rIface.errorBindingMap[errors.Unauthorized{}.Error()] = errors.UnauthorizedBindingType()
+	errDef2, errError2 := typeConverter.ConvertToDataDefinition(errors.UnauthorizedBindingType())
+	if errError2 != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultReplicaDiskCollectionsClient.get method's errors.Unauthorized error - %s",
+			bindings.VAPIerrorsToError(errError2).Error())
+		return nil
+	}
+	errorDefinitions = append(errorDefinitions, errDef2.(data.ErrorDefinition))
+	rIface.errorBindingMap[errors.NotFound{}.Error()] = errors.NotFoundBindingType()
+	errDef3, errError3 := typeConverter.ConvertToDataDefinition(errors.NotFoundBindingType())
+	if errError3 != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultReplicaDiskCollectionsClient.get method's errors.NotFound error - %s",
+			bindings.VAPIerrorsToError(errError3).Error())
+		return nil
+	}
+	errorDefinitions = append(errorDefinitions, errDef3.(data.ErrorDefinition))
+
+	methodDefinition := core.NewMethodDefinition(methodIdentifier, input, output, errorDefinitions)
+	return &methodDefinition
+}
---
+++ services/vmc/draas/DefaultTaskClient.go
@@ -0,0 +1,139 @@
+
+/* Copyright © 2019 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: BSD-2-Clause */
+
+// Code generated. DO NOT EDIT.
+
+/*
+ * Client stubs for service: Task
+ * Functions that implement the generated TaskClient interface
+ */
+
+
+package draas
+
+import (
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/lib/vapi/std/errors"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/services/vmc/draas/model"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/bindings"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/core"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/data"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/lib"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/log"
+	"gitlab.eng.vmware.com/golangsdk/vsphere-automation-sdk-go/runtime/protocol/client"
+)
+
+type DefaultTaskClient struct {
+	interfaceName       string
+	interfaceDefinition core.InterfaceDefinition
+	methodIdentifiers   []core.MethodIdentifier
+	methodNameToDefMap  map[string]*core.MethodDefinition
+	errorBindingMap     map[string]bindings.BindingType
+	interfaceIdentifier core.InterfaceIdentifier
+	connector           client.Connector
+}
+
+func NewDefaultTaskClient(connector client.Connector) *DefaultTaskClient {
+	interfaceName := "com.vmware.vmc.draas.task"
+	interfaceIdentifier := core.NewInterfaceIdentifier(interfaceName)
+	methodIdentifiers := []core.MethodIdentifier{
+		core.NewMethodIdentifier(interfaceIdentifier, "get"),
+	}
+	interfaceDefinition := core.NewInterfaceDefinition(interfaceIdentifier, methodIdentifiers)
+	errorBindingMap := make(map[string]bindings.BindingType)
+	errorBindingMap[errors.InternalServerError{}.Error()] = errors.InternalServerErrorBindingType()
+	errorBindingMap[errors.InvalidArgument{}.Error()] = errors.InvalidArgumentBindingType()
+	errorBindingMap[errors.OperationNotFound{}.Error()] = errors.OperationNotFoundBindingType()
+	errorBindingMap[errors.UnexpectedInput{}.Error()] = errors.UnexpectedInputBindingType()
+	errorBindingMap[errors.ServiceUnavailable{}.Error()] = errors.ServiceUnavailableBindingType()
+	errorBindingMap[errors.TimedOut{}.Error()] = errors.TimedOutBindingType()
+	tIface := DefaultTaskClient{interfaceName: interfaceName, methodIdentifiers: methodIdentifiers, interfaceDefinition: interfaceDefinition, errorBindingMap: errorBindingMap, interfaceIdentifier: interfaceIdentifier, connector: connector}
+	tIface.methodNameToDefMap = make(map[string]*core.MethodDefinition)
+	tIface.methodNameToDefMap["get"] = tIface.getMethodDefinition()
+	return &tIface
+}
+
+func (tIface *DefaultTaskClient) Get(orgParam string, taskParam string) (model.Task, error) {
+	typeConverter := tIface.connector.TypeConverter()
+	methodIdentifier := core.NewMethodIdentifier(tIface.interfaceIdentifier, "get")
+	sv := bindings.NewStructValueBuilder(taskGetInputType(), typeConverter)
+	sv.AddStructField("Org", orgParam)
+	sv.AddStructField("Task", taskParam)
+	inputDataValue, inputError := sv.GetStructValue()
+	if inputError != nil {
+		var emptyOutput model.Task
+		return emptyOutput, bindings.VAPIerrorsToError(inputError)
+	}
+	operationRestMetaData := taskGetRestMetadata()
+	connectionMetadata := map[string]interface{}{lib.REST_METADATA: operationRestMetaData}
+	tIface.connector.SetConnectionMetadata(connectionMetadata)
+	methodResult := tIface.Invoke(tIface.connector.NewExecutionContext(), methodIdentifier, inputDataValue)
+	var emptyOutput model.Task
+	if methodResult.IsSuccess() {
+		output, errorInOutput := typeConverter.ConvertToGolang(methodResult.Output(), taskGetOutputType())
+		if errorInOutput != nil {
+			return emptyOutput, bindings.VAPIerrorsToError(errorInOutput)
+		}
+		return output.(model.Task), nil
+	} else {
+		methodError, errorInError := typeConverter.ConvertToGolang(methodResult.Error(), tIface.errorBindingMap[methodResult.Error().Name()])
+		if errorInError != nil {
+			return emptyOutput, bindings.VAPIerrorsToError(errorInError)
+		}
+		return emptyOutput, methodError.(error)
+	}
+}
+
+
+func (tIface *DefaultTaskClient) Invoke(ctx *core.ExecutionContext, methodId core.MethodIdentifier, inputDataValue data.DataValue) core.MethodResult {
+	methodResult := tIface.connector.GetApiProvider().Invoke(tIface.interfaceName, methodId.Name(), inputDataValue, ctx)
+	return methodResult
+}
+
+
+func (tIface *DefaultTaskClient) getMethodDefinition() *core.MethodDefinition {
+	interfaceIdentifier := core.NewInterfaceIdentifier(tIface.interfaceName)
+	typeConverter := tIface.connector.TypeConverter()
+
+	input, inputError := typeConverter.ConvertToDataDefinition(taskGetInputType())
+	output, outputError := typeConverter.ConvertToDataDefinition(taskGetOutputType())
+	if inputError != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultTaskClient.get method's input - %s",
+			bindings.VAPIerrorsToError(inputError).Error())
+		return nil
+	}
+	if outputError != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultTaskClient.get method's output - %s",
+			bindings.VAPIerrorsToError(outputError).Error())
+		return nil
+	}
+	methodIdentifier := core.NewMethodIdentifier(interfaceIdentifier, "get")
+	errorDefinitions := make([]data.ErrorDefinition, 0)
+	tIface.errorBindingMap[errors.Unauthenticated{}.Error()] = errors.UnauthenticatedBindingType()
+	errDef1, errError1 := typeConverter.ConvertToDataDefinition(errors.UnauthenticatedBindingType())
+	if errError1 != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultTaskClient.get method's errors.Unauthenticated error - %s",
+			bindings.VAPIerrorsToError(errError1).Error())
+		return nil
+	}
+	errorDefinitions = append(errorDefinitions, errDef1.(data.ErrorDefinition))
+	tIface.errorBindingMap[errors.Unauthorized{}.Error()] = errors.UnauthorizedBindingType()
+	errDef2, errError2 := typeConverter.ConvertToDataDefinition(errors.UnauthorizedBindingType())
+	if errError2 != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultTaskClient.get method's errors.Unauthorized error - %s",
+			bindings.VAPIerrorsToError(errError2).Error())
+		return nil
+	}
+	errorDefinitions = append(errorDefinitions, errDef2.(data.ErrorDefinition))
+	tIface.errorBindingMap[errors.NotFound{}.Error()] = errors.NotFoundBindingType()
+	errDef3, errError3 := typeConverter.ConvertToDataDefinition(errors.NotFoundBindingType())
+	if errError3 != nil {
+		log.Errorf("Error in ConvertToDataDefinition for DefaultTaskClient.get method's errors.NotFound error - %s",
+			bindings.VAPIerrorsToError(errError3).Error())
+		return nil
+	}
+	errorDefinitions = append(errorDefinitions, errDef3.(data.ErrorDefinition))
+
+	methodDefinition := core.NewMethodDefinition(methodIdentifier, input, output, errorDefinitions)
+	return &methodDefinition
+}
